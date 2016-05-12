54f99df772fe923c08b3ecd2d5c51215b7016f2c
Ben Langfeld <ben@langfeld.me>
2016-05-12 18:13:37 -0300

    Restricted volumes don't have autosize info

--- lib/puppet/provider/netapp_volume/cmode.rb
+++ lib/puppet/provider/netapp_volume/cmode.rb
@@ -153,7 +153,7 @@ Puppet::Type.type(:netapp_volume).provide(:cmode, :parent => Puppet::Provider::N
         vol_export_policy = vol_export_attributes.child_get_string("policy")
       end
       # Get Auto size settings.
-      if vol_state != 'offline'
+      unless %w{offline restricted}.include?('offline')
         vol_auto_size = vol_autosize_info.child_get_string("mode")
       end
 
