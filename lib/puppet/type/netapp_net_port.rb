5980fde632ac06eb172f702a76b549c14206c351
Nidhi.Hada <hnidhi@netapp.com>
2017-08-30 07:44:33 -0400

    Unit Test Case added

--- lib/puppet/type/netapp_net_port.rb
+++ lib/puppet/type/netapp_net_port.rb
@@ -7,8 +7,7 @@ Puppet::Type.newtype(:netapp_net_port) do
     desc 'The node and port name concatenated with @'
     isnamevar
     validate do |value|
-      raise ArgumentError, "#{value} is a invalid node_port name" unless
-        value =~ /^[a-zA-Z\-_.0-9]+(?:@[0-9a-zA-Z]+)$/
+      raise ArgumentError, '%s is an invalid node_port name.' % value unless value =~ /^[a-zA-Z\-_.0-9]+(?:@[0-9a-zA-Z]+)$/
     end
   end
 
