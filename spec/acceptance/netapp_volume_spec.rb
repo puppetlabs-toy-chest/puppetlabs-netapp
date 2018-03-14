67e63857268f6efd1d9fb3c2cca6112e6b91cb96
Nidhi.Hada <hnidhi@netapp.com>
2018-03-14 01:54:01 -0400

    Improved acceptance test by adding more test cases.

--- spec/acceptance/netapp_volume_spec.rb
+++ spec/acceptance/netapp_volume_spec.rb
@@ -8,7 +8,7 @@ node 'vsim-01' {
 node 'vserver-01' {
   netapp_volume {'volume_with_param':
     aggregate        => 'aggr1',
-    ensure           => present,
+    ensure           => 'present',
     volume_type      => 'rw',
     group_id         => '0',
     user_id          => '0',
@@ -21,15 +21,86 @@ node 'vserver-01' {
     run_device(:allow_changes => false)
   end
 
-  it 'edit a volume' do
+  it 'modifies a volume' do
     pp=<<-EOS
 node 'vsim-01' {
 }
 node 'vserver-01' {
-  netapp_volume {'volumename':
+  netapp_volume {'volume_with_param':
     aggregate => 'aggr1',
-    ensure => present,
+    ensure => 'present',
     snapshot_policy => 'default-1weekly',
+    snapreserve     => '70',
+    autosize        => 'grow'
+
+  }
+}
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
+  it 'mounts a volume' do
+    pp=<<-EOS
+node 'vsim-01' {
+}
+node 'vserver-01' {
+  netapp_volume {'volume_with_param':
+    aggregate    => 'aggr1',
+    ensure       => 'present',
+    junctionpath => '/vol'
+  }
+}
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
+  it 'unmounts a volume' do
+    pp=<<-EOS
+node 'vsim-01' {
+}
+node 'vserver-01' {
+  netapp_volume {'volume_with_param':
+    aggregate    => 'aggr1',
+    ensure       => 'present',
+    junctionpath => 'false'
+  }
+}
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
+  it 'makes a volume offline' do
+    pp=<<-EOS
+node 'vsim-01' {
+}
+node 'vserver-01' {
+  netapp_volume {'volume_with_param':
+    aggregate => 'aggr1',
+    state     => 'offline',
+    ensure    => 'present',
+  }
+}
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+  end
+
+  it 'makes a volume online' do
+    pp=<<-EOS
+node 'vsim-01' {
+}
+node 'vserver-01' {
+  netapp_volume {'volume_with_param':
+    aggregate => 'aggr1',
+    ensure    => 'present',
+    state     => 'online'
   }
 }
     EOS
@@ -43,10 +114,10 @@ node 'vserver-01' {
 node 'vsim-01' {
 }
 node 'vserver-01' {
-  netapp_volume {'vol_offline':
+  netapp_volume {'volume_with_param':
     aggregate => 'aggr1',
     state  => 'offline',
-    ensure => present,
+    ensure => 'present',
   }
 }
     EOS
@@ -58,10 +129,10 @@ node 'vserver-01' {
 node 'vsim-01' {
 }
 node 'vserver-01' {
-  netapp_volume {'vol_offline':
+  netapp_volume {'volume_with_param':
     aggregate => 'aggr1',
     state  => 'offline',
-    ensure => absent,
+    ensure => 'absent',
   }
 }
     EOS
